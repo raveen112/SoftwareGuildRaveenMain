64826c33bfee40f8bd39bc905b3f0a9bc48d8bc3
Raveen Mathai <raveensmathai@gmail.com>
2021-08-26 21:13:12 -0400

    first code

---
+++ M1/JavaClassroom/HelloWorld/src/main/java/raveenM/helloword/Helllo.java
@@ -0,0 +1,14 @@
+/*
+ *@author Raveen Mathai
+ *email: raveensmathai@gmail.com
+ *date: currentdate
+ *purpose: My first Hello World program in an IDE
+ */
+package raveenM.helloword;
+
+public class Helllo {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+    
+}
