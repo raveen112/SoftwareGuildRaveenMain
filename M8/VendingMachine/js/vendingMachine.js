e187f89989c1e68b8145ebbde5ed6da8ebdad479
Raveen Mathai <raveensmathai@gmail.com>
2022-05-17 12:10:22 -0400

    Vending Machine in progress git commit -m "Vending Machine in progress"

--- M8/VendingMachine/js/vendingMachine.js
+++ M8/VendingMachine/js/vendingMachine.js
@@ -1,54 +1,90 @@
-$( document ).ready(function() {
-   loadItems();
+$(document).ready(function () {
+    loadItems();
+    $("#totalMoneyField").val('0.00');
 });
 
-function loadItems(){
-    $('.item').empty();
+function loadItems() {
 
-    var itemList = $('.item');
+    $('#item').empty();
+    var itemDisplay = $('#item');
 
     $.ajax({
         type: 'GET',
         url: 'http://vending.us-east-1.elasticbeanstalk.com/items',
 
-        success: function(itemArray) {
-            $.each(itemArray, function(index, item){
-                var itemId= item.id;
-                var name = item.name;
-                var price = item.price.toFixed(2);
-                var quantity = item.quantity;
-
-                var items = '<div class="items col-md-3" id="item'+itemId+'" onclick="displayId('+itemId+')"><p>';
-                items += itemId;
-                items += '<br>';
-                items += '<div style = "text-align:center">' + name;
-                items += '<br>';
-                items += '<div style = "margin-top:10px">';
-                items += "$" + price;
-                items += '<br><br>';
-                items += "Quantity Left: " + quantity;
-                items += '</div></p></div>';
-
-                itemList.append(items);
-            })
+        success: function(vendItems){
+				$.each(vendItems, function (index, item) {
+					displayItem = $("#itemTemplate").clone();
+					displayItem.removeClass("d-none");
+					displayItem.find("#itemNumber").text(item.id);
+					displayItem.attr("onclick", "displayId(" + item.id + ")");
+					displayItem.find("#itemName").text(item.name);
+					displayItem.find("#itemPrice").text("$"+ item.price.toFixed(2));
+					displayItem.find("#itemQuantity").text("Quantity Left: " + item.quantity);
+					itemDisplay.append(displayItem);
+				});
         },
-        error: function() {
+        error: function () {
             $('#errorMessages')
                 .append($('<li>')
-                .attr({class: 'list-group-item list-group-item-danger'})
-                .text('Error calling web service. Please try again later.'));
+                    .attr({ class: 'list-group-item list-group-item-danger' })
+                    .text('Error calling web service. Please try again later.'));
         }
     });
 
 }
-    function displayId(itemId) {
-        // When an item is clicked on have its ID be displayed in the items field.
-         $("#itemField").val(itemId);
-         // Add the item ID to the item IdHolderDiv.
-         $('#itemIds').val(itemId);
-         // Reset the change, if any change is being returned.
-         $("#changeField").val('');
-      }
-      
+function displayId(itemId) {
+    // When an item is clicked on have its ID be displayed in the items field.
+    $("#itemField").val(itemId);
+    // Add the item ID to the item IdHolderDiv.
+    $('#itemIds').val(itemId);
+    // Reset the change, if any change is being returned.
+    $("#changeField").val('');
+}
 
+// Button functionality 
+// 1$ 
+$("#addDollar").on("click", function(){
 
+    var currentVal = $("#totalMoneyField").val();
+    var newVal = Number(currentVal) + 1.00;
+    $("#totalMoneyField").val(newVal.toFixed(2));
+
+})
+
+// 0.5$
+$("#addQuarter").on("click", function(){
+
+    var currentVal = $("#totalMoneyField").val();
+    var newVal = Number(currentVal) + .5;
+    $("#totalMoneyField").val(newVal.toFixed(2));
+
+})
+
+// 0.05$
+$("#addDime").on("click", function(){
+
+    var currentVal = $("#totalMoneyField").val();
+    var newVal = Number(currentVal) + .05;
+    $("#totalMoneyField").val(newVal.toFixed(2));
+
+})
+
+// 0.01$
+$("#addNickle").on("click", function(){
+
+    var currentVal = $("#totalMoneyField").val();
+    var newVal = Number(currentVal) + .01;
+    $("#totalMoneyField").val(newVal.toFixed(2));
+
+})
+
+// method to display the change after a purchase is made 
+function initialChange(){
+    
+}
+
+// method return change
+function changeReturn(){
+
+}
