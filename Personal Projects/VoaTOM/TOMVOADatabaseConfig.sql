6afcd7c6b91d49ca6700454b7d25877c510d692f
raveen112 <raveensmathai@gmail.com>
2022-08-19 13:48:53 -0400

    TOM Form added

--- Personal Projects/VoaTOM/TOMVOADatabaseConfig.sql
+++ Personal Projects/VoaTOM/TOMVOADatabaseConfig.sql
@@ -9,6 +9,7 @@ login VARCHAR(12) NOT NULL,
 `name` VARCHAR(50) NOT NULL,
 issue_id INT
 );
+
 CREATE TABLE issues(
 issue_id INT PRIMARY KEY AUTO_INCREMENT, 
 complaint VARCHAR(255) NOT NULL,
@@ -24,6 +25,7 @@ REFERENCES associate (id)
 INSERT INTO associate VALUES(1, "nomaan","Nomaan Muhammad", "2");
 INSERT INTO issues VALUES(1, "This is a really tough job","2022-01-03", true, 1);
 INSERT INTO issues VALUES(2, "Need safety shoes","2022-01-06", false, 1);
+
 
 
 -- SELECT * FROM associate a
